790b97242f6422c08b02b2fa19a85bb00305f7c1
Ayush Devendra Tomar <41674634+ayushtom@users.noreply.github.com>
2021-03-23 23:12:04 +0530

    final

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const InfoCard = () => {
+  return (
+    <div elevation={3} style={{ textAlign: 'center', padding: '0 10%' }}>
+      Add and manage your expenses!
+    </div>
+  );
+};
+
+export default InfoCard;
